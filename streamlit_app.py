7847ef7846a2a01fbf22208f25e942e60a68d192
DonnaD1628 <109257847+DonnaD1628@users.noreply.github.com>
2022-07-16 23:46:02 -0500

    Update streamlit_app.py
    pre-populated the pick list with a few fruits as an example for the customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # add a picklist so that they can pick the fruit for their smoothis
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries')
 
 # display the fruit list
 streamlit.dataframe(my_fruit_list)
